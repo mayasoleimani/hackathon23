fcc4c4e9305b030c582b20bf6bf492a06e4df419
Mihir Gupta <69476837+gupta-mihir@users.noreply.github.com>
2023-03-11 22:54:58 -0500

    second push

--- UndoCO2/Assets/Scripts/MainMenu.cs
+++ UndoCO2/Assets/Scripts/MainMenu.cs
@@ -2,9 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public bool easyDone = false;
+    public bool normalDone = false;
     // Start is called before the first frame update
     public void PlayGame()
     {
@@ -16,4 +19,45 @@ public class MainMenu : MonoBehaviour
         Debug.Log("Quit!");
         Application.Quit();
     }
+    public Button mediumBtn;
+    public Button brutalBtn;
+    
+    public void Easy()
+	{
+        SceneManager.LoadScene(2);
+        easyDone = true;
+
+	}
+   
+    public void Medium()
+    {
+        //SceneManagement.LoadScene(2);
+    }
+
+    public void Brutal()
+	{
+        //SceneManagement.LoadScene(3)
+	}
+
+    public void Update()
+	{
+        if (easyDone)
+		{
+            mediumBtn.enabled = true;
+		}
+        else
+		{
+            mediumBtn.enabled = false;
+		}
+        
+        if (normalDone)
+		{
+            brutalBtn.enabled = true;
+		}
+		else
+		{
+            brutalBtn.enabled = false;
+		}
+	}
+
 }
